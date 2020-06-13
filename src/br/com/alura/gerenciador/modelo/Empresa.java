4e5e8314aa4c9be8c386d9aaf1dbb071c13cbaa0
augusto <augustosouzads@gmail.com>
2020-06-13 14:09:02 -0300

    re-adicionei o package modelo ao projeto e organizei os imports nas vlasses - AULA 1 atvidade 6

--- src/br/com/alura/gerenciador/modelo/Empresa.java
+++ src/br/com/alura/gerenciador/modelo/Empresa.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.modelo;
 
 import java.util.Date;
 
@@ -8,12 +8,15 @@ public class Empresa {
 	private String nome;
 	private Date dataAbertura = new Date();
 	
+	
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
+	
 	public String getNome() {
 		return nome;
 	}
@@ -21,11 +24,11 @@ public class Empresa {
 		this.nome = nome;
 	}
 	
-	public void setDataAbertura(Date dataAbertura) {
-		this.dataAbertura = dataAbertura;
-	}
 	
 	public Date getDataAbertura() {
 		return dataAbertura;
+	}
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;		
 	}
 }
