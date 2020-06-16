b855e27f3676357ee67a06c67cfbe68cb36848fc
augusto <augustosouzads@gmail.com>
2020-06-16 01:47:31 -0300

    implementei a interface açao e adicionei as classes  açoes modifiquei o servlet para um codigo mais O.O deixando o controlador mais generico como o conceito de  padrao comand

--- src/br/com/alura/gerenciador/acao/NovaEmpresa.java
+++ src/br/com/alura/gerenciador/acao/NovaEmpresa.java
@@ -12,9 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-public class NovaEmpresa {
+public class NovaEmpresa implements Acao{
 	
-	public String executa(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+	public String executa (HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 		
 System.out.println("Cadastrando nova empresa");
 		
