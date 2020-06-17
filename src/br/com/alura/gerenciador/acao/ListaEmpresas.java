543d64fc3ca85db3d2b71584ce56a1d164899194
augusto <augustosouzads@gmail.com>
2020-06-17 01:07:49 -0300

    criei o filtro MonitoramntoFilter para monitorar a o tempo de execução de cada ação

--- src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -2,24 +2,17 @@ package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
 public class ListaEmpresas implements Acao{
 	
 	public String executa(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession sessao = request.getSession();
-		if(sessao.getAttribute("usuarioLogado") == null){
-			return "redirect:entrada?acao=LoginForm";
-		}
 		
 		System.out.println("acao Listando empresas");
 		
